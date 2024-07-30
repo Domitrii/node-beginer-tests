08cfea83137eaa674705af48f4ff1f71132b7df3
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-07-30 21:32:37 +0100

    userName test

--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 export const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 export const authSchema = Joi.object({
+  name: Joi.string().default("User"),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
   repeatPassword: Joi.string().min(6).required(),
