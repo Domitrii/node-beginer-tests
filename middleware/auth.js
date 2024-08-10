59b66014b99ef9ac792d8024070f6db321efee4e
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-08-10 16:31:34 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,34 @@
-import jwt from 'jsonwebtoken';
-import User from '../modules/usersModule.js';
+import jwt from 'jsonwebtoken'
+import User from '../modules/usersModule.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
-function auth(req, res, next) {
-    const {authorization = ""} = req.headers;
+function auth(req, res, next){
+    const {authorization = ""} = req.headers
 
-    const [bearer, token] = authorization.split(' ', 2);
-    if (bearer !== "Bearer") {
-        return res.status(401).send({message: "Bearer is not defined or token is missing"});
+    const [bearer, token] = authorization.split(' ', 2)
+    if(bearer !== "Bearer"){
+       return res.status(401).send({message: "Bearer is not defined"})
     }
 
-    jwt.verify(token, process.env.SECRET_PASS, async (err, decode) => {
-        if (err) {
-            return res.status(401).send({message: "U have a verify problem"});
+    jwt.verify(token, process.env.SECRET_PASS , async (err, decode) => {
+        if(err){
+            return res.status(401).send({message: "U have a verify problem"})
         }
-        try {
-            const user = await User.findById(decode.id);
+        try{
+            const user = await User.findById(decode.id)
 
-            if (!user || user.token !== token) {
-                return res.status(401).send({message: "Invalid token2"});
+            if(!user || user.token !== token){
+                return res.status(401).send({message: "Invalid token2", user})
             }
 
-            req.user = {id: decode.id};
-            next();
-        } catch (error) {
-            next(error);
+            req.user = {id: decode.id}
+            next()
+        } catch (error){
+            next(error)
         }
-    });
+    })
+
 }
 
-export default auth;
+export default auth
