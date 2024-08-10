f2eef5696d22f705b303b5e17482e86ad5af103f
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-08-10 12:31:39 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,37 +1,35 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+import User from '../modules/usersModule.js';
 
-import User from '../modules/usersModule.js'
+function auth(req, res, next) {
+    const authorizationHeader = req.headers.authorization;
 
-function auth(req, res, next){
-    const authorizationHeader = req.headers.authorization
-
-    // if(typeof authorizationHeader === 'undefined'){
-    //     return res.status(401).send({message: "Invalid token1"})
-    // }
-
-    const [bearer, token] = authorizationHeader.split(' ', 2)
-    if(bearer !== "Bearer"){
-       return res.status(401).send({message: "Bearer is not defined"})
+    if (!authorizationHeader) {
+        return res.status(401).send({message: "Invalid token1"});
     }
 
-    jwt.verify(token, process.env.SECRET_PASS , async (err, decode) => {
-        if(err){
-            return res.status(401).send({message: "U have a verify problem"})
-        }
-        try{
-            const user = await User.findById(decode.id)
+    const [bearer, token] = authorizationHeader.split(' ', 2);
+    if (bearer !== "Bearer" || !token) {
+        return res.status(401).send({message: "Bearer is not defined or token is missing"});
+    }
 
-            if(!user || user.token !== token){
-                return res.status(401).send({message: "Invalid token2", user})
+    jwt.verify(token, process.env.SECRET_PASS, async (err, decode) => {
+        if (err) {
+            return res.status(401).send({message: "U have a verify problem"});
+        }
+        try {
+            const user = await User.findById(decode.id);
+
+            if (!user || user.token !== token) {
+                return res.status(401).send({message: "Invalid token2", user});
             }
 
-            req.user = {id: decode.id}
-            next()
-        } catch (error){
-            next(error)
+            req.user = {id: decode.id};
+            next();
+        } catch (error) {
+            next(error);
         }
-    })
-
+    });
 }
 
-export default auth
+export default auth;
