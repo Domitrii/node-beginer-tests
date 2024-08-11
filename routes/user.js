9b38555d0c7f6a0a12ee81f0524ed85335b27e22
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-08-11 20:34:48 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/register', UserControllers.register);
 router.post('/login', UserControllers.login);
-router.post('/logout', UserControllers.logout);
+router.post('/logout', authMiddleware, UserControllers.logout);
 // router.post('/logout', authMiddleware, UserControllers.logout);
 // router.post('/refresh', authRefresh, UserControllers.refresh )
 // router.get('/updatePassword', authMiddleware, UserControllers.updatePassword)
