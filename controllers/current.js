88765fa5d90a657d014106aefd49d7283eb3e70d
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-08-18 16:53:08 +0100

    without email

--- controllers/current.js
+++ controllers/current.js
@@ -28,12 +28,11 @@ async function currentUser(req, res, next){
 
 async function updateUser(req, res, next) {
     try {
-        const {gender, name, dailyNorm, timeActive, weight, email} = req.body
+        const {gender, name, dailyNorm, timeActive, weight} = req.body
 
         const updateData = {
             ...(gender && {gender}),
             ...(name && {name}),
-            ...(email && {email}),
             ...(dailyNorm && {dailyNorm}),
             ...(timeActive && {timeActive}),
             ...(weight && {weight})
