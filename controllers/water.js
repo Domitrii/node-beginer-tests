bfc8d828debf242bfe6bef031d2f768d08967771
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-08-26 12:15:16 +0100

    Update water.js

--- controllers/water.js
+++ controllers/water.js
@@ -60,6 +60,7 @@ async function getWaterRecordByMonth(req, res, next){
 
 async function deleteWaterRecord(req, res, next){
     try{
+        console.log(req.params)
         const {id} = req.params
         console.log(id)
         if(!isValidObjectId(id)) throw HttpError(401, "We can't find this id")
